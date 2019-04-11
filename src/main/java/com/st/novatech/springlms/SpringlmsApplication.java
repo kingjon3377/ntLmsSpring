c9bdaedbef84f302ba41218d1fd3565789ad500c
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-04-11 14:35:24 -0400

    initialize Spring project

---
+++ src/main/java/com/st/novatech/springlms/SpringlmsApplication.java
@@ -0,0 +1,13 @@
+package com.st.novatech.springlms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringlmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringlmsApplication.class, args);
+	}
+
+}
