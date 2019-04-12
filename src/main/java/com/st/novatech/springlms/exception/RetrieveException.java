f86b6a482f89f9851d24bf8eda5d83772187e809
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-12 17:34:32 -0400

    Mark RetrieveException with @ResponseStatus so it becomes HTTP 404 when thrown.

--- src/main/java/com/st/novatech/springlms/exception/RetrieveException.java
+++ src/main/java/com/st/novatech/springlms/exception/RetrieveException.java
@@ -1,5 +1,8 @@
 package com.st.novatech.springlms.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  * An exception class to report the failure of a retrieval operation from the
  * service layer to the application layer.
@@ -8,6 +11,7 @@ package com.st.novatech.springlms.exception;
  * @author Jonathan Lovelace
  */
 @SuppressWarnings("serial")
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class RetrieveException extends TransactionException {
 	/**
 	 * To throw an instance of this exception class, the caller must supply the
