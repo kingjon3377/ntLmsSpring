07ff2543c3514d0ba0201a7738d0d17c342dd549
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-12 15:30:58 -0400

    CatalogController: Add Javadoc comments to methods.

--- src/main/java/com/st/novatech/springlms/controller/CatalogController.java
+++ src/main/java/com/st/novatech/springlms/controller/CatalogController.java
@@ -28,16 +28,32 @@ public final class CatalogController {
 	@Autowired
 	private AdministratorService service;
 
+	/**
+	 * Get all authors from the database. Spring turns the list into JSON (or XML?).
+	 * @return the list of all authors in the database.
+	 * @throws TransactionException on internal error.
+	 */
 	@RequestMapping({"/authors", "/authors/"})
 	public List<Author> getAuthor() throws TransactionException {
 		return service.getAllAuthors();
 	}
 
+	/**
+	 * Get all books from the database. Spring turns the list into JSON (or XML?).
+	 * @return the list of all books in the database
+	 * @throws TransactionException on internal error
+	 */
 	@RequestMapping({"/books", "/books/"})
 	public List<Book> getBooks() throws TransactionException {
 		return service.getAllBooks();
 	}
 
+	/**
+	 * Get an author by its ID number.
+	 * @param authorId the ID number of the author
+	 * @return the author
+	 * @throws TransactionException if author not found, or on internal error
+	 */
 	@RequestMapping({"/author/${uthorId}", "/author/{authorId}/"})
 	public Author getAuthor(@PathVariable("authorId") final int authorId) throws TransactionException {
 		final Author author = service.getAuthor(authorId);
@@ -48,6 +64,13 @@ public final class CatalogController {
 		}
 	}
 
+	/**
+	 * Update an author by ID number.
+	 * @param authorId the ID number of the author to update
+	 * @param input the author data to update.
+	 * @return the updated author
+	 * @throws TransactionException if author not found, or on internal error
+	 */
 	@RequestMapping(path = { "/author/{authorId}",
 			"/author/{authorId}/" }, method = RequestMethod.PUT)
 	public Author updateAuthor(@PathVariable("authorId") final int authorId,
@@ -62,11 +85,22 @@ public final class CatalogController {
 		}
 	}
 
+	/**
+	 * Create an author with the given name.
+	 * @param name the name to give the author
+	 * @return the created author
+	 * @throws TransactionException on internal error
+	 */
 	@RequestMapping(path = {"/author", "/author/" }, method = RequestMethod.POST)
 	public Author createAuthor(@RequestParam("name") final String name) throws TransactionException {
 		return service.createAuthor(name);
 	}
 
+	/**
+	 * Delete the author with the given ID.
+	 * @param authorId the ID of the author to delete.
+	 * @throws TransactionException on internal error
+	 */
 	@RequestMapping(path = {"/author/{authorId}", "/author/{authorId}"}, method = RequestMethod.DELETE)
 	public void deleteAuthor(@PathVariable("authorId") final int authorId) throws TransactionException {
 		final Author author = service.getAuthor(authorId);
