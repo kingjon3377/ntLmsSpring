649025e2aa489e4903f08cc09ef621b470546b20
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-12 14:01:10 -0400

    CatalogController: Add a few more basic routes.

--- src/main/java/com/st/novatech/springlms/controller/CatalogController.java
+++ src/main/java/com/st/novatech/springlms/controller/CatalogController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.st.novatech.springlms.exception.RetrieveException;
 import com.st.novatech.springlms.exception.TransactionException;
 import com.st.novatech.springlms.model.Author;
 import com.st.novatech.springlms.model.Book;
@@ -37,18 +38,40 @@ public final class CatalogController {
 		return service.getAllBooks();
 	}
 
+	@RequestMapping({"/author/${uthorId}", "/author/{authorId}/"})
+	public Author getAuthor(@PathVariable("authorId") final int authorId) throws TransactionException {
+		final Author author = service.getAuthor(authorId);
+		if (author == null) {
+			throw new RetrieveException("Author not found");
+		} else {
+			return author;
+		}
+	}
+
 	@RequestMapping(path = { "/author/{authorId}",
 			"/author/{authorId}/" }, method = RequestMethod.PUT)
 	public Author updateAuthor(@PathVariable("authorId") final int authorId,
 			@RequestBody final Author input) throws TransactionException {
-		final Author author = new Author(authorId, input.getName());
-		service.updateAuthor(author); // TODO: what if no author with that ID yet? Should create ...
-		// TODO: get author by ID rather than returning user input
-		return author;
+		final Author author = service.getAuthor(authorId);
+		if (author == null) {
+			throw new RetrieveException("Author not found");
+		} else {
+			author.setName(input.getName());
+			service.updateAuthor(author);
+			return service.getAuthor(authorId);
+		}
 	}
 
 	@RequestMapping(path = {"/author", "/author/" }, method = RequestMethod.POST)
 	public Author createAuthor(@RequestParam("name") final String name) throws TransactionException {
 		return service.createAuthor(name);
+	}
+
+	@RequestMapping(path = {"/author/{authorId}", "/author/{authorId}"}, method = RequestMethod.DELETE)
+	public void deleteAuthor(@PathVariable("authorId") final int authorId) throws TransactionException {
+		final Author author = service.getAuthor(authorId);
+		if (author != null) {
+			service.deleteAuthor(author);
+		}
 	}
 }
