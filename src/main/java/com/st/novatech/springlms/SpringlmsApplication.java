a1d5a71c93331ade6dee00a28aa624891f18d1f6
Jonathan Lovelace <kingjon3377@gmail.com>
2019-04-17 11:09:53 -0400

    SpringlmsApplication: Add Javadoc comments.

--- src/main/java/com/st/novatech/springlms/SpringlmsApplication.java
+++ src/main/java/com/st/novatech/springlms/SpringlmsApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
+/**
+ * Main driver class for the application for running from the command line.
+ */
 @PropertySource("classpath:database-config.properties")
 @SpringBootApplication
 public class SpringlmsApplication {
 
+	/**
+	 * Main method.
+	 * @param args parsed by Spring
+	 */
 	public static void main(final String[] args) {
 		SpringApplication.run(SpringlmsApplication.class, args);
 	}
